7bf83fd05152361aeba875117e56ae5fd8ce347e
Taichi Sato <okms310gmail.com>
2018-10-23 16:12:51 +0900

    add AddressableAssets
    Add Sample

---
+++ Assets/Main/Core/Sample/SampleLoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.U2D;
+
+public class SampleLoad : MonoBehaviour {
+
+	[AssetReferenceTypeRestriction(typeof(SpriteAtlas)),SerializeField]
+	AssetReference playerRef;
+
+	public int direction = 0;
+	[SerializeField]  SpriteRenderer spRenderer;
+
+	void Start(){
+		Addressables.LoadAsset<SpriteAtlas>(playerRef).Completed += atlas => {
+			spRenderer.sprite = atlas.Result.GetSprite(atlas.Result.name + "_" + direction.ToString());
+		};
+	}
+}
