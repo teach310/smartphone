ccd3b313f783687aa2cf090c3decd314b7ef6586
大山亮 <oyameycry3024@gmail.com>
2018-10-30 12:42:50 +0900

    feat:シーン遷移の仕組み制作しました

--- Assets/Main/Game/Scripts/GameControllers/SceneChanger.cs
+++ Assets/Main/Game/Scripts/GameControllers/SceneChanger.cs
@@ -5,21 +5,35 @@ using UnityEngine;
 public class SceneChanger : MonoBehaviour {
 
 	[SerializeField]
-	GameObject ScenesObject;
+	private GameObject ScenesObject;
 
-	GameObject[] scenes;
+	private GameObject[] scenes;
+
+	private GameObject currentScene = null;
 
 	// Use this for initialization
 	void Awake () {
 
 		int i = 0;
+		scenes = new GameObject[ScenesObject.transform.childCount];
 		foreach (Transform child in ScenesObject.transform)
 		{
 			scenes[i] = child.gameObject;
-			Debug.Log(scenes[i]);
 			i++;
+		}
+		currentScene = scenes[0];
+		SceneChange(scenes[0].transform.name);
+	}
 
-			
+	//シーン遷移させる関数
+	public void SceneChange(string scene_name){
+
+		for(int i = 0; i < scenes.Length; i++){
+			if(scenes[i].transform.name == scene_name){
+				currentScene.SetActive(false);
+				scenes[i].SetActive(true);
+				currentScene = scenes[i];
+			}
 		}
 
 	}
