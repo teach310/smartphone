2a5ffd93742fa429d513ff91b76672b7792dbebc
大山亮 <oyameycry3024@gmail.com>
2018-11-13 12:53:36 +0900

    feat:Mainで３、２、１スタート演出実装

---
+++ Assets/Main/Game/Scripts/GameControllers/Scene/Main.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : SceneBase {
+
+	[SerializeField]
+	private int _count=0;
+	[SerializeField]
+	private Text countdownText;
+
+	private bool isControl;
+
+	void Start(){
+		StartCoroutine(CountDown(_count));
+	}
+
+	private IEnumerator CountDown(int count) {  
+		countdownText.text = count.ToString();
+		isControl = false;
+        while(true){
+			 yield return new WaitForSeconds (1f);
+			 count--;
+
+			 if(count>0){
+				 countdownText.text = count.ToString();
+			 }
+			 else if(count==0){
+				 countdownText.text = "Start";
+			 }
+			 else{
+				Destroy(countdownText.gameObject);
+				isControl = true;
+				yield break;
+			 }
+
+			 yield return null;
+
+		}
+    }  
+
+}
