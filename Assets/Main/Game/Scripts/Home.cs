67e989588737240475b0f3981f2c03db489a6549
Taichi Sato <okms310gmail.com>
2019-01-08 13:13:02 +0900

    TitleからHomeへ

---
+++ Assets/Main/Game/Scripts/Home.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CA2;
+using UnityEngine;
+
+namespace Wakame {
+    public class Home : CA2.SceneBase {
+        
+    }
+}
---
+++ Assets/Main/Game/Scripts/Title.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Async;
+using CA2;
+
+namespace Wakame {
+    public class Title : CA2.SceneBase {
+        public void GoHome() => SimpleSceneNavigator.Instance.GoForwardAsync<Home>().Forget();
+    }
+}
