22bae825a6ba1d3fb856a80100a68d1129f7e1b7
大山亮 <oyameycry3024@gmail.com>
2019-01-08 12:21:12 +0900

    feat:途中進捗あげ

---
+++ Assets/Main/Game/Scripts/GameControllers/Test/PlayerTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.U2D;
+
+public class PlayerTest : MonoBehaviour {
+
+	[AssetReferenceTypeRestriction(typeof(SpriteAtlas)),SerializeField]
+	AssetReference playerRef;
+
+	[SerializeField]  SpriteRenderer spRenderer;
+	private int currentDirection = 0;
+	private List<Sprite> directionSprites = new List<Sprite>();
+
+	enum direction{
+		UP = 0,
+		DOWN = 1,
+		LEFT = 2,
+		RIGHT = 3
+	};
+	direction DIRECTION;
+
+	void Start(){
+		
+		Addressables.LoadAsset<SpriteAtlas>(playerRef).Completed += atlas => {
+			//spRenderer.sprite = atlas.Result.GetSprite(atlas.Result.name + "_" + direction.ToString());
+
+			for(int i = 0; i < 3; i++){
+				directionSprites.Add(atlas.Result.GetSprite(atlas.Result.name + "_" + i.ToString()));
+				Debug.Log(directionSprites[i]);
+			}
+			
+		};
+	}
+
+	void Update(){
+
+		InputDirection();
+		UpdateDirection();
+	}
+
+	// void FixedUpdate(){
+
+	// }
+
+	private void InputDirection(){
+		
+		if(Input.GetKey(KeyCode.DownArrow)){
+			currentDirection = 0;
+		}
+
+		if(Input.GetKey(KeyCode.UpArrow)){
+			currentDirection = 1;
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			currentDirection = 2;
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow)){
+			currentDirection = 3;
+		}
+	}
+
+	private void UpdateDirection(){
+		
+		if(currentDirection == 3){
+			spRenderer.sprite = directionSprites[2];
+			spRenderer.flipX = true;
+		}
+		else{
+			spRenderer.sprite = directionSprites[currentDirection];
+			spRenderer.flipX = false;
+		}
+	}
+}
---
+++ Assets/Main/Game/Scripts/GameControllers/Test/StageLoadTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using Wakame;
+
+namespace Wakame.Data
+{
+
+	public class StageLoadTest : MonoBehaviour {
+		
+		void Start(){
+			
+		}
+
+	}
+
+}
