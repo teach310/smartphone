169c94ee7b6089c76be0a73871e0537468c56006
Taichi Sato <okms310gmail.com>
2019-04-16 13:00:24 +0900

    feat Load

--- Assets/Main/Game/Scripts/Home.cs
+++ Assets/Main/Game/Scripts/Home.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using CA2;
 using UnityEngine;
 using UniRx.Async;
+using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+using UniRx;
 
 namespace Wakame {
     public class Home : CA2.SceneBase {
+        
+        [SerializeField] Image sampleImage;
+        [SerializeField] SpriteRenderer sampleSprite;
+        Sprite tile0;
 
         public override void OnLoad(object options = null){
-            Debug.Log(Domain.Repository.StageDataRepository.FindById(1));
-            var keyValuePair = CA2.Data.KeyValueRepository.FindAll () [0];
-			Debug.LogFormat ("Key {0}, Value{1}", keyValuePair.key, keyValuePair.value);
+            // Debug.Log(Domain.Repository.StageDataRepository.FindById(1));
+            // var keyValuePair = CA2.Data.KeyValueRepository.FindAll () [0];
+			// Debug.LogFormat ("Key {0}, Value{1}", keyValuePair.key, keyValuePair.value);
+            Addressables.LoadAsset<Sprite>("Tile/floor_0").Completed += sp => {
+                tile0 = sp.Result;
+                sampleImage.sprite = tile0;
+                sampleSprite.sprite = tile0;
+            };
         }
 
         public void GoGame(){
